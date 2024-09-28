fbb3f37103aeec3eb376bcb6971f0b7b478b6a64
Owen Brady <Ocean@owenbrady.net>
2024-09-27 20:40:48 -0500

    Initial Creation of Web Project to allow editing of the Browsercap sql lite DB.

---
+++ Ocean.Core.BrowserDetective.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ocean.Core.BrowserDetective.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
